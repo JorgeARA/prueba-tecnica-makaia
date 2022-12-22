5d5e870afd24f03a804e4f74d04c6e637c50f43f
Jorge Restrepo <jorge.psi22@gmail.com>
2022-12-22 15:47:57 -0500

    estilos login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,6 +7,10 @@ import './login.css';
 function Login() {
   return (
     <Container className='login_container'>
+      <header>
+        <img src="https://firebasestorage.googleapis.com/v0/b/prueba-tecnica-makaia.appspot.com/o/images%2FprismaFinal.png?alt=media&token=48c3b15d-e7e1-4b6e-b842-b40369b21595" alt="logo prisma" />
+        <hr />
+    </header>
         <Form className='login_form'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Correo electrónico</Form.Label>
